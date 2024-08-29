a9538a206960a866505c78a9358ddf70ee4fe97f
TORY-SKY <ezevictor559@gmail.com>
2024-08-29 13:55:45 +0100

    sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import LoginPage from "./LoginPage";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SideBar from "./SideBar";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+    errorElement: "Error Accessing this page",
+  },
+  {
+    path: "/recoverPassword",
+    element: "recover password page",
+    errorElement: "Error Accessing this page",
+  },
+  {
+    path: "/sidebar",
+    element: <SideBar />,
+    errorElement: "Error Accessing this page",
+  },
+]);
 function App() {
   return (
     <>
-      <LoginPage />
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/TopNavbar.tsx
+++ src/TopNavbar.tsx
@@ -1,25 +1,28 @@
+import { Link } from "react-router-dom";
 const TopNavbar = () => {
   return (
-    <div>
-      <nav>
-        <div>
-          <svg
-            width="8"
-            height="12"
-            viewBox="0 0 8 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M6.39998 10.7998L1.59998 5.9998L6.39998 1.19981"
-              stroke="#4169E1"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          <span>Home</span>
-        </div>
+    <div className="Navbar-container">
+      <nav className="navigation-bar">
+        <Link to="/">
+          <div className="homensvg">
+            <svg
+              width="8"
+              height="12"
+              viewBox="0 0 8 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6.39998 10.7998L1.59998 5.9998L6.39998 1.19981"
+                stroke="#4169E1"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <span className="">Home</span>
+          </div>
+        </Link>
         <button className="sign-up-btn">Sign Up</button>
       </nav>
     </div>
