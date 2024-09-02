226dd8530048e6b3a1c950540abe971c1e633ba1
TORY-SKY <ezevictor559@gmail.com>
2024-09-02 22:25:26 +0100

    auth

--- src/Authentication/Firebase.ts
+++ src/Authentication/Firebase.ts
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAm17L8BKqfiMYdCc8IK_XqKbDpptI_WiU",
-  authDomain: "subsum-252ac.firebaseapp.com",
-  projectId: "subsum-252ac",
-  storageBucket: "subsum-252ac.appspot.com",
-  messagingSenderId: "708024221052",
-  appId: "1:708024221052:web:158ddfdf6450b1812db8f9",
-  measurementId: "G-C63RCCRT7Z"
+  
 };
 
 // Initialize Firebase
