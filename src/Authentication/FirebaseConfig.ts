879dbbe25ebc74fae51fe9094c8260e58c49621e
TORY-SKY <ezevictor559@gmail.com>
2024-09-10 23:55:07 +0100

    quick fix after 24hrs

--- src/Authentication/FirebaseConfig.ts
+++ src/Authentication/FirebaseConfig.ts
@@ -1,16 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAm17L8BKqfiMYdCc8IK_XqKbDpptI_WiU",
+  authDomain: "subsum-252ac.firebaseapp.com",
+  projectId: " subsum-252ac",
+  storageBucket: "subsum-252ac.appspot.com",
+  messagingSenderId: "708024221052",
+  appId: "1:708024221052:web:158ddfdf6450b1812db8f9",
+  measurementId: "G-C63RCCRT7Z",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
+// import.meta.env.VITE_FIREBASE_API_KEY
+// import.meta.env.VITE_FIREBASE_AUTH_DOMAIN
+// import.meta.env.VITE_FIREBASE_PROJECT_ID
+// import.meta.env.VITE_FIREBASE_STORAGE_BUCKET
+// import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID
+// import.meta.env.VITE_FIREBASE_APP_ID
+// import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
